09013e596c1c70a23ca92c52823a65d09ce8c72a
Jordan Reed <jordanreed03@gmail.com>
2020-12-26 22:08:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,9 +2,33 @@
 
 class School 
   attr_accessor :name
+  attr_writer :sort
+  
   
   def initialize(name)
     @name = name 
+    @roster = { }
+  end
+  
+  def add_student(stuname, grade)
+     if roster.has_key?(grade)
+       roster[grade] << stuname
+     else 
+       roster[grade] = [stuname]
+     end 
   end 
+
+  def roster 
+    @roster
+  end 
+
+  def grade(num)
+    return roster[num] 
+  end 
+  
+  def sort 
+    roster[grade].sort
+  end 
+
 end 
  
